031a273c0670a5566af443cd7248b5d10a30a446
serenahack <serenahackart@gmail.com>
2018-03-07 09:28:26 +1100

    Initial Project

---
+++ Brackeys Tutorial 01/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        // Debug.Log(collisionInfo.collider.name);
+
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            // Debug.Log("We hit an obstacle.");
+
+            movement.enabled = false;
+
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
